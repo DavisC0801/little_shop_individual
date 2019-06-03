c9cd6e5a4312c93aadc5683b542d143222b20fd7
davisc0801 <davisc1014@gmail.com>
2019-06-03 14:29:01 -0600

    Add address factory and modify the user factory to remove the address information

---
+++ spec/factories/address.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :address, class: Address do
+    sequence(:address) { |n| "Address #{n}" }
+    sequence(:city) { |n| "City #{n}" }
+    sequence(:state) { |n| "State #{n}" }
+    sequence(:zip) { |n| "Zip #{n}" }
+  end
+end
