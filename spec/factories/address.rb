ae55e408196a183288024a042c7304bab844bf3d
davisc0801 <davisc1014@gmail.com>
2019-06-04 13:04:14 -0600

    Update address factory

--- spec/factories/address.rb
+++ spec/factories/address.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :address, class: Address do
-    sequence(:address) { |n| "Address #{n}" }
+    sequence(:street) { |n| "Street #{n}" }
     sequence(:city) { |n| "City #{n}" }
     sequence(:state) { |n| "State #{n}" }
     sequence(:zip) { |n| "Zip #{n}" }
