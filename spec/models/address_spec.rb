69b5dd4abd21251cc51cf35b3b5c3bb2071e720e
davisc0801 <davisc1014@gmail.com>
2019-06-04 12:54:27 -0600

    Update model test for addresses

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Address, type: :model do
   describe 'validations' do
     it { should validate_presence_of :nickname }
-    it { should validate_presence_of :address }
+    it { should validate_presence_of :street }
     it { should validate_presence_of :city }
     it { should validate_presence_of :state }
     it { should validate_presence_of :zip }
@@ -11,6 +11,15 @@ RSpec.describe Address, type: :model do
 
   describe 'relationships' do
     it { should belong_to :user }
-    it { should belong_to :order }
+    it { should have_many :orders }
+  end
+
+  describe "instance methods" do
+    it "#used?" do
+      address = create(:address)
+      expect(address.used?).to be_falsy
+      create(:order, address: address, status: 2)
+      expect(address.used?).to be_truthy
+    end
   end
 end
