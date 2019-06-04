16eb90faf49ebe3a150ba4fd2856bb964aa7d085
davisc0801 <davisc1014@gmail.com>
2019-06-04 13:07:32 -0600

    Update address and order models

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,11 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
-  belongs_to :order, optional: true
+  has_many :orders, dependent: :destroy
 
-  validates_presence_of :nickname, :address, :city, :state, :zip
+  validates_presence_of :nickname, :street, :city, :state, :zip
+
+  def used?
+    completed_order_count = orders.where("orders.status = 1 or orders.status = 2").count
+    completed_order_count > 0
+  end
 end
